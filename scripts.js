2376d32b96e5644ef57ac5b162daaa0b6242a98f
Ivan Zaytsev <ivanzaytsev@ivans-air.lan>
2025-03-02 20:14:13 -0500

    fix

--- scripts.js
+++ scripts.js
@@ -45,3 +45,15 @@ const translations = {
     }
   }
   
+  // Adding toggle functionality for checklist
+  const checkboxes = document.querySelectorAll('.check-item');
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+      if (this.checked) {
+        this.nextElementSibling.style.textDecoration = 'line-through';
+      } else {
+        this.nextElementSibling.style.textDecoration = 'none';
+      }
+    });
+  });
+  
